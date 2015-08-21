75109ddb5badb6940a6d0e8a2b55351bc3664b07
Masudul Haque <saadixl@outlook.com>
2015-08-21 20:39:53 +0600

    Reverse Geo Coding implemenetd in backend.
    App is now dynamic

--- src/app.js
+++ src/app.js
@@ -1,30 +1,43 @@
 var UI = require('ui');
 var ajax = require('ajax');
+
 // Create a Card with title and subtitle
 var card = new UI.Card({
   title:'Waqt',
   subtitle:'Fetching...',
   style:'small'
 });
+ var cityAjaxUrl;
 
 // Display the Card
 card.show();
 
-// Construct URL
-var URL = 'http://app.saadixl.pw/pebblewaqt/msa.php';
 
+var locationOptions = {
+  enableHighAccuracy: true, 
+  maximumAge: 10000, 
+  timeout: 10000
+};
+
+function locationSuccess(pos) {
+  cityAjaxUrl =  pos.coords.latitude.toString() + ',' + pos.coords.longitude.toString();
+  console.log(cityAjaxUrl);
+  cityAjaxUrl = cityAjaxUrl.toString();
+  // Construct URL
+var URL = 'http://app.saadixl.pw/pebblewaqt/rgc.php?'+cityAjaxUrl;
+console.log(URL);
 // Make the request
-ajax(
+ ajax(
   {
     url: URL,
     type: 'json'
   },
   function(data) {
     // Success!
-    console.log("Successfully fetched waqt data!");
+    console.log("waqt data loaded!");
 
     // Extract data
-    var location = 'Location: Dhaka';
+    var location = 'Location: '+data.query;
     var desc = 'Fajr\t: ' + data.items[0].fajr+ '\nDhuhr: ' +  data.items[0].dhuhr + '\nAsr: '  + data.items[0].asr+ '\nMaghrib: '  + data.items[0].maghrib+ '\nIsha: '  + data.items[0].isha;
     card.subtitle(location);
     card.body(desc);
@@ -33,4 +46,14 @@ ajax(
     // Failure!
     console.log('Failed fetching weather data: ' + error);
   }
-);
+); 
+
+}
+
+function locationError(err) {
+  console.log('location error (' + err.code + '): ' + err.message);
+}
+
+navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+
+
